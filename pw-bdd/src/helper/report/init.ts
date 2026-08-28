7a77b2ef08a6f4110b99158e3ae67f16f769d86e
bmifsud <bernardmifsud@gmail.com>
2026-08-28 14:38:02 +0200

    Update pw-bdd/src/helper/report/init.ts
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- pw-bdd/src/helper/report/init.ts
+++ pw-bdd/src/helper/report/init.ts
@@ -1,8 +1,10 @@
 const fs = require("fs-extra");
 
-try {
-    fs.ensureDir("test-results");
-    fs.emptyDir("test-results");
-} catch (error) {
-    console.log("Folder not created! " + error);
-}
+(async () => {
+    try {
+        await fs.ensureDir("test-results");
+        await fs.emptyDir("test-results");
+    } catch (error) {
+        console.log("Folder not created! " + error);
+    }
+})();
